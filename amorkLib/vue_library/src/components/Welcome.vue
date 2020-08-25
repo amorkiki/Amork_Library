73b2baa899cb87a4907c24dd4bb670a04c559239
amorJTW <amorkikixxx@163.com>
2020-08-25 22:51:52 +0800

    userlist finished

---
+++ amorkLib/vue_library/src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container>
+    <div>welcome @_@</div>
+  </el-container>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.el-container {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  background: url(../assets/8.jpeg) no-repeat;
+  background-size: cover;
+  > div {
+    padding: 5px;
+    font-size: 50px;
+    font-family: Marker Felt;
+    color: #fff;
+  }
+}
+</style>
